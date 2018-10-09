bcb1d6e42815856f0ad0ddfa94234b88f81e2448
Timothy Dao <timster78811@gmail.com>
2018-10-09 12:28:34 -0400

    did a thing

--- drills.js
+++ drills.js
@@ -13,25 +13,38 @@ const BinarySearchTree = require('./binaryTree');
 //   2   5    9
 //          7
 
-const input = [3, 1, 4, 6, 9, 2, 5, 7];
+const input = [3, 1, 4, 6, 9, 2, 5, 7, 8];
 const BST = new BinarySearchTree();
 input.forEach(num => BST.insert(num));
-// console.log(BST);
+console.log(BST);
 
-const findHeight = function(tree) {
-  let currentNode = tree;
-  let leftSide = 0;
-  let rightSide = 0;
+function height(searchTree) {
+  //base case
+  if (!searchTree || !searchTree.key) {
+    return 0;
+  }
 
-  if (currentNode.left === null && currentNode.right === null) {
-    return 1;
-  }
-  if (currentNode.left !== null) {
-    return (leftSide = 1 + findHeight(currentNode.left));
-  }
-  if (currentNode.right !== null) {
-    return (rightSide = 1 + findHeight(currentNode.right));
-  }
-  return (height = leftSide >= rightSide ? leftSide : rightSide);
-};
-console.log(findHeight(BST));
+  const leftDepth = height(searchTree.left);
+  const rightDepth = height(searchTree.right);
+
+  return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
+
+console.log(height(BST));
+// const findHeight = function(tree) {
+//   let currentNode = tree;
+//   let leftSide = 0;
+//   let rightSide = 0;
+
+//   if (currentNode.left === null && currentNode.right === null) {
+//     return 1;
+//   }
+//   if (currentNode.left !== null) {
+//     leftSide = 1 + findHeight(currentNode.left);
+//   }
+//   if (currentNode.right !== null) {
+//     rightSide = 1 + findHeight(currentNode.right);
+//   }
+//   return (height = leftSide >= rightSide ? leftSide : rightSide);
+// };
+// console.log(findHeight(BST));
